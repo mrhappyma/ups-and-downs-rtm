7905d120dcc36a440409b164add417e70e6e98bb
Dominic Ruggiero <dominic@userexe.me>
2024-07-18 20:09:59 -0400

    new untested delete/edit detection

--- src/watch-message.ts
+++ src/watch-message.ts
@@ -1,4 +1,4 @@
-import { App, GenericMessageEvent } from "@slack/bolt";
+import { App } from "@slack/bolt";
 import env from "./util/env.js";
 import { WebClient } from "@slack/web-api";
 import { ScrewedUpReason, screwedUp } from "./index.js";
@@ -18,23 +18,23 @@ export const setLatestMessage = (
 };
 
 export const initWatchLatestMessage = (bolt: App, modern: WebClient) => {
-//  setInterval(async () => {
-//    if (!ts || !text || !user) return;
-//    const messages = (
-//      await modern.conversations.history({
-//        latest: ts,
-//        channel: env.CHANNEL,
-//        limit: 1,
-//        inclusive: true,
-//      })
-//    ).messages;
-//    const message = messages ? messages[0] : undefined;
-//    if (message?.ts != ts) {
-//      screwedUp(ScrewedUpReason.deleted, user, ts);
-//      setLatestMessage(undefined, undefined, undefined);
-//    } else if (message?.text != text) {
-//      screwedUp(ScrewedUpReason.edited, user, ts);
-//      setLatestMessage(undefined, undefined, undefined);
-//    }
-//  }, 1500);
+  setInterval(async () => {
+    if (!ts || !text || !user) return;
+    const messages = (
+      await modern.conversations.history({
+        latest: ts,
+        channel: env.CHANNEL,
+        limit: 5,
+        inclusive: true,
+      })
+    ).messages;
+    const message = messages?.find((m) => m.ts === ts && m.text === text);
+    if (!message) {
+      screwedUp(ScrewedUpReason.deleted, user, ts);
+      setLatestMessage(undefined, undefined, undefined);
+    } else if (message.text !== text) {
+      screwedUp(ScrewedUpReason.edited, user, ts);
+      setLatestMessage(undefined, undefined, undefined);
+    }
+  }, 1500);
 };
